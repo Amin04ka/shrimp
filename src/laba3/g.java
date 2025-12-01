b874ccf11593e97229b74bcf8d732dc8038b1903
Amin04ka <aminamahmudova630@gmail.com>
2025-12-01 19:01:21 +0500

    commit

--- src/laba3/g.java
+++
@@ -1,9 +0,0 @@
-package laba3;
-
-public class g {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-
-    }
-}
--- src/laba3/v.java
+++
@@ -1,9 +0,0 @@
-package laba3;
-
-public class v {
-    public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-
-    }
-}
